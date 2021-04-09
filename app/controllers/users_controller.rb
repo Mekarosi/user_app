07a2b37efebe325d3daf5443b6c20a8c3bd0bf34
Mekarosi <mekarosi_assa@yahoo.com>
2021-04-09 20:26:58 +0100

    Add Bootstrap modal attribute and adjusted controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+     @user = User.new
   end
 
   # GET /users/1 or /users/1.json
@@ -27,9 +28,11 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
+        format.js   { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.js  { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
